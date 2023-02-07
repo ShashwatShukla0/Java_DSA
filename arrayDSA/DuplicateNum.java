d3a78209efd2b0083f6ba2fb7e0fba050d2284d1
ShashwatShukla0 <sskrish786@gmail.com>
2023-02-07 12:16:01 +0530

    Commit

---
+++ arrayDSA/DuplicateNum.java
@@ -0,0 +1,28 @@
+package arrayDSA;
+
+import java.util.Scanner;
+
+public class DuplicateNum {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size and array: ");
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(" Duplicates: ");
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+
+        sc.close();
+    }
+    
+}
