a9e36341f18568e6b5962e0a7f22229b9dcf54ad
ShashwatShukla0 <sskrish786@gmail.com>
2023-02-14 23:29:38 +0530

    Commit

---
+++ collectionDSA/CallStudent.java
@@ -0,0 +1,20 @@
+package collectionDSA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallStudent {
+
+    Student s1 = new Student(1,"abc","12th","english");
+    Student s2 = new Student(2,"def","11th","hindi");
+    Student s3 = new Student(3,"ghi","11th","maths");
+    Student s4 = new Student(4,"jkl","10th","english");
+    Student s5 = new Student(5,"mno","10th","physics");
+    Student s6 = new Student(6,"pqr","12th","computer");
+
+    List<Student> student = new ArrayList<>();
+
+    student.add(s1);
+
+    
+}
---
+++ collectionDSA/Student.java
@@ -0,0 +1,59 @@
+package collectionDSA;
+
+public class Student {
+
+    int id;
+
+    String name;
+
+    String classn;
+
+    String subject;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassn() {
+        return classn;
+    }
+
+    public void setClassn(String classn) {
+        this.classn = classn;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", classn=" + classn + ", subject=" + subject + "]";
+    }
+
+    Student(int id,String name,String classn,String subject){
+        this.id=id;
+        this.name=name;
+        this.classn=classn;
+        this.subject=subject;
+    }
+    
+}
