82239a947c8cc6010c894ec60296d7890ebe3797
ShashwatShukla0 <sskrish786@gmail.com>
2023-02-15 22:37:57 +0530

    Commit

--- collectionDSA/CallStudent.java
+++ collectionDSA/CallStudent.java
@@ -2,6 +2,7 @@ package collectionDSA;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CallStudent {
 
@@ -12,9 +13,27 @@ public class CallStudent {
     Student s5 = new Student(5,"mno","10th","physics");
     Student s6 = new Student(6,"pqr","12th","computer");
 
-    List<Student> student = new ArrayList<>();
+    static List<Student> st= new ArrayList<>();
 
-    student.add(s1);
+    public void data(){
 
-    
+        st.add(s1);
+        st.add(s2);
+        st.add(s3);
+        st.add(s4);
+        st.add(s5);
+        st.add(s6);
+    }
+
+    static List<Student> asc = st.stream().sorted().collect(Collectors.toList());
+
+    static int n = asc.size();
+
+    public static void main(String args[]){
+        for(int i=0;i<n;i++){
+            System.out.println(asc);
+        }
+    }
+
+
 }
