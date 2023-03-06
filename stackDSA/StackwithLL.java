9f4f1d6a127254a2f53141823c51c12074df7335
ShashwatShukla0 <sskrish786@gmail.com>
2023-03-06 20:34:56 +0530

    Commit

---
+++ stackDSA/StackwithLL.java
@@ -0,0 +1,68 @@
+package stackDSA;
+
+public class StackwithLL {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node( int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack{
+        Node head;
+
+        public boolean isEmpty(){
+            return head==null;
+        }
+
+        public void push(int data){
+            Node new_node = new Node(data);
+            if(isEmpty()){
+                head = new_node;
+            }
+
+            new_node.next = head;
+            head = new_node;
+        }
+
+        public int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return head.data;
+        }
+
+        public static void main(String args[]){
+
+            Stack st = new Stack();
+    
+            st.push(1);
+            st.push(2);
+            st.push(3);
+            st.push(4);
+    
+            while(!st.isEmpty()){
+                System.out.println(st.peek());
+                st.pop();
+            }
+        }
+    }
+
+    
+    
+}
