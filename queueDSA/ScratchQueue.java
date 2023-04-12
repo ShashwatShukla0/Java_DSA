a133226459b964120e37c07c149e47386e5c8eef
ShashwatShukla0 <sskrish786@gmail.com>
2023-04-12 15:01:43 +0530

    Commit

--- queueDSA/ScratchQueue.java
+++ queueDSA/ScratchQueue.java
@@ -17,13 +17,61 @@ public class ScratchQueue {
         this.front = this.rear =null;
     }
 
+    boolean isEmpty(){
+        return front == null & rear == null;
+    }
+
+    int peek(){
+        if(isEmpty()){
+            System.out.println("Queue is empty: ");
+            return -1;
+        }
+        return front.data;
+    }
+
     void enque(int data){
         Node node = new Node(data);
 
-        if(rear = null){
-            rear = front = null;
+        if(this.rear == null){
+            this.rear = this.front = null;
+            return;
         }
+
+        this.rear.next = node;
+        this.rear = node;
         
+    }
+
+    int deque(){
+        if(this.front == null){
+            return -1;
+        }
+
+        int node = this.front.data;
+        this.front = this.front.next;
+
+        if(this.front == null){
+            this.rear = null;
+        }
+        return node;
+    }
+
+    public static void main(String[] args){
+
+        ScratchQueue q = new ScratchQueue();
+        q.enque(1);
+        q.enque(2);
+        q.enque(3);
+        q.enque(4);
+        q.enque(5);
+        q.enque(6);
+        q.enque(7);
+        q.enque(8);
+        
+        while(!q.isEmpty()){
+            System.out.println("Queue is: "+ q.peek());
+            q.deque();
+        }
     }
     
 }
