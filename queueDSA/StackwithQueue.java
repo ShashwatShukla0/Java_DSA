4d6ab6c4e8338b53f24d10138d90848316a31e31
ShashwatShukla0 <sskrish786@gmail.com>
2023-04-13 14:05:34 +0530

    Commit

--- queueDSA/StackwithQueue.java
+++ queueDSA/StackwithQueue.java
@@ -28,8 +28,38 @@ public class StackwithQueue {
 
     }
 
-    public static void main(String[] args){
+    static void pop(){
+        if(q1.isEmpty()){
+            return;
+        }
+        q1.remove();
+    }
 
+    static int top(){
+        if(q1.isEmpty()){
+            return -1;
+        }
+        return q1.peek();
+    }
+
+    static int size(){
+        return q1.size();
+    }
+
+    public static void main(String[] args){
+        Queue s = new Queue();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println("current size: " + s.size());
+        System.out.println(s.top());
+        s.pop();
+        System.out.println(s.top());
+        s.pop();
+        System.out.println(s.top());
+ 
+        System.out.println("current size: " + s.size());
     }
     
 }
